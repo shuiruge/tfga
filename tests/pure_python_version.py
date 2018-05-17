a11c27143cb61cb03db12baeab8b2c6657636cec
shuiruge <shuiruge@whu.edu.cn>
2018-05-17 22:42:52 +0800

    Finished and tested the  "pure python version".

--- tests/pure_python_version.py
+++ tests/pure_python_version.py
@@ -9,16 +9,16 @@ import numpy as np
 class Generation(object):
     """Auxillary class."""
 
-    def __init__(self, population, log_fitnesses=None):
+    def __init__(self, population, fitnesses=None):
         """
         Args:
             population: Numpy array with shape `[n_individuals] + `
                 `individual_shape`.
-            log_fitnesses: Numpy array with shape `[n_individuals]`, or
+            fitnesses: Numpy array with shape `[n_individuals]`, or
                 `None` as the initialized state, optional.
         """
         self.population = population
-        self.log_fitnesses = log_fitnesses
+        self.fitnesses = fitnesses
 
 
 class BaseGeneticAlgorithm(abc.ABC):
@@ -26,16 +26,25 @@ class BaseGeneticAlgorithm(abc.ABC):
     def __init__(self,
                  n_individuals,
                  crossover_probability,
-                 mutation_probability):
+                 mutation_probability,
+                 verbose=False):
         self.n_individuals = n_individuals
         self.crossover_probability = crossover_probability
         self.mutation_probability = mutation_probability
-        self._probabilities = None  # as cache.
+        self.verbose = verbose
+
+        # Initialize
+        self.n_generations = 1
 
     def train(self):
         generation = self.initialize_generation()
-        while not self.shall_shop(generation):
+        while not self.shall_stop(generation):
             generation = self.create_next_generation(generation)
+            self.n_generations += 1
+            if self.verbose:
+                msg = 'Generation {0}, with the best fitness {1}'
+                best_fitness = np.max(generation.fitnesses)
+                print(msg.format(self.n_generations, best_fitness))
         return generation
 
     def create_next_generation(self, generation):
@@ -51,8 +60,8 @@ class BaseGeneticAlgorithm(abc.ABC):
         next_generation = self.crossover(next_generation)
         next_generation = self.mutate(next_generation)
         # Fitness
-        next_log_fitnesses = self.get_log_fitnesses(next_generation.population)
-        next_generation.log_fitnesses = next_log_fitnesses
+        next_fitnesses = self.get_fitnesses(next_generation.population)
+        next_generation.fitnesses = next_fitnesses
         return next_generation
 
     def select(self, generation):
@@ -62,16 +71,16 @@ class BaseGeneticAlgorithm(abc.ABC):
         Returns:
             An instance of `Generation`.
         """
-        self._probabilities = softmax(generation.log_fitnesses)
-        n_selected = (1 - self.crossover_probability) * self.n_individuals
+        probabilities = softmax(generation.fitnesses)
+        n_selected = int((1-self.crossover_probability) * self.n_individuals)
         permutation = np.random.choice(
-            np.arrange(self.n_individuals, dtype='int32'),
+            self.n_individuals,
             size=n_selected,
-            p=self._probabilities,
-            replace=False)
+            p=probabilities,
+            replace=True)
         population = generation.population[permutation]
-        log_fitnesses = generation.log_fitnesses[permutation]
-        return Generation(population, log_fitnesses)
+        fitnesses = generation.fitnesses[permutation]
+        return Generation(population, fitnesses)
         
     def crossover(self, generation):
         """
@@ -86,18 +95,21 @@ class BaseGeneticAlgorithm(abc.ABC):
 
         # Select individuals for crossover
         n_selected = 2 * int(self.crossover_probability*self.n_individuals/2)
+        probabilities = softmax(generation.fitnesses)
         permutation = np.random.choice(
-            np.arrange(n_individuals, dtype='int32'),
+            n_individuals,
             size=n_selected,
-            p=self._probabilities)
+            p=probabilities)
         selected = generation.population[permutation]
 
         # Get pairs as the shape `[2, n_selected/2] + individual_shape`
-        individual_shape = selected.shape[1:]
-        pairs = selected.reshape([2, n_selected/2]+individual_shape)
+        individual_shape = list(selected.shape[1:])
+        pairs = np.reshape(selected, [2, n_selected//2]+individual_shape)
 
         # Make crossover
-        crossovered = [self._crossover(a, b) for a, b in zip(pairs[0], pairs[1])]
+        crossovered = np.array(
+            [self._crossover(a, b) for a, b in zip(pairs[0], pairs[1])])
+        crossovered = np.reshape(crossovered, [n_selected]+individual_shape)
         population = np.concatenate((generation.population, np.array(crossovered)),
                                     axis=0)
         return Generation(population)
@@ -109,7 +121,8 @@ class BaseGeneticAlgorithm(abc.ABC):
             individual_a: Numpy array with the shape `individual_shape`.
             individual_b: Numpy array with the shape `individual_shape`.
         Returns:
-          Numpy array with the shape `individual_shape`.
+            Tuple of two numpy arraies, both with the shape
+            `individual_shape`.
         """
         pass
 
@@ -120,10 +133,14 @@ class BaseGeneticAlgorithm(abc.ABC):
         Returns:
             An instance of `Generation`.
         """
-        new_population = np.array([
-            self._mutate(individual) for individual in generation.population
-        ])
-        return Generation(new_population, generation.log_fitnesses)
+        new_population = []
+        for individual in generation.population:
+            if np.random.random() < self.mutation_probability:
+                new_population.append(self._mutate(individual))
+            else:
+                new_population.append(individual)
+        new_population = np.array(new_population)
+        return Generation(new_population, generation.fitnesses)
 
     @abc.abstractmethod
     def _mutate(self, individual):
@@ -131,19 +148,29 @@ class BaseGeneticAlgorithm(abc.ABC):
         Args:
             individual: Numpy array with the shape `individual_shape`.
         Returns:
-          Numpy array with the shape `individual_shape`.
+            Numpy array with the shape `individual_shape`.
         """
         pass
 
+    def get_fitnesses(self, population):
+        """
+        Args:
+            population: Numpy array with shape `[n_individuals] + `
+                `individual_shape`.
+        Returns:
+            Numpy array with shape `[n_individuals]`.
+        """
+        return np.array([self.get_fitness(_) for _ in population])
+
     @abc.abstractmethod
-    def get_log_fitnesses(self, population):
+    def get_fitness(self, individual):
         pass
 
-    def initialize_generation(self, generation):
+    def initialize_generation(self):
         """Returns an instance of `Generation`."""
         population = self.initialize_population()
-        log_fitnesses = self.get_log_fitnesses(population)
-        return Generation(population, log_fitnesses)
+        fitnesses = self.get_fitnesses(population)
+        return Generation(population, fitnesses)
 
     @abc.abstractmethod
     def initialize_population(self):
@@ -152,3 +179,83 @@ class BaseGeneticAlgorithm(abc.ABC):
     @abc.abstractmethod
     def shall_stop(self, generation):
         pass
+
+
+def softmax(x):
+    """
+    Args:
+        x: Numpy array.
+    Returns:
+        Numpy array.
+    """
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+
+if __name__ == '__main__':
+
+
+    import time
+
+
+    class SimpleGeneticAlgorithm(BaseGeneticAlgorithm):
+
+        def __init__(self,
+                     n_individuals,
+                     crossover_probability,
+                     mutation_probability,
+                     **kwargs):
+            super().__init__(n_individuals,
+                             crossover_probability,
+                             mutation_probability,
+                             **kwargs)
+
+        def get_fitness(self, x):
+            fitness = np.sin(2 * np.pi * x)
+            if x.shape:
+                return np.mean(fitness)
+            else:
+                return fitness
+
+        def _mutate(self, x):
+            noise_scale = 0.2
+            new_x = x + np.random.normal() * noise_scale
+            condition = np.logical_and(new_x > np.zeros_like(x),
+                                       new_x < np.ones_like(x))
+            return np.where(condition, new_x, np.random.random(size=x.shape))
+
+        def _crossover(self, x1, x2):
+            return (x1 + (x2 - x1) * np.random.random(),
+                    x1 + (x2 - x1) * np.random.random())
+
+        def initialize_population(self):
+            population = [
+                np.random.random([100]) for _ in range(self.n_individuals)
+            ]
+            population = np.array(population, dtype='float32')
+            print('Initialized population with shape', population.shape)
+            return population
+
+        def shall_stop(self, generation):
+            max_fitness = np.max(generation.fitnesses)
+            if max_fitness > 1 - 1e-2:
+                return True
+            else:
+                return False
+
+    def main():
+        time_start = time.time()
+
+        sga = SimpleGeneticAlgorithm(10**2, 0.3, 0.1, verbose=True)
+        final_generation = sga.train()
+        best_fitness = np.max(final_generation.fitnesses)
+
+        time_elipsed = time.time() - time_start
+
+        msg = ('After {0} generations, {1} seconds elipsed, '
+               'the best fitness is gained as {2}')
+        msg = msg.format(sga.n_generations, time_elipsed, best_fitness)
+        print(msg)
+
+    main()
+
